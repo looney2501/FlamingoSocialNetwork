15a6adc5f1ab3278cc258188aa6582dd9741dddf
Mircea-George Cozarev <mircea2501@gmail.com>
2022-01-12 01:51:49 +0200

    Created password hashing algorithm

---
+++ src/main/java/com/map_toysocialnetwork_gui/Utils/PasswordHasing/MD5Hashing.java
@@ -0,0 +1,17 @@
+package com.map_toysocialnetwork_gui.Utils.PasswordHasing;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public interface MD5Hashing {
+    default String hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest m = MessageDigest.getInstance("MD5");
+        m.update(password.getBytes());
+        byte[] bytes = m.digest();
+        StringBuffer stringBuffer = new StringBuffer();
+        for (byte b:bytes) {
+            stringBuffer.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        }
+        return stringBuffer.toString();
+    }
+}
